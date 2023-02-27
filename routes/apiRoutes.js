37b5ff85279168db0c318278ea9178c804ead860
Cesar Castillo <castillocesar2412@gmail.com>
2023-02-27 17:12:07 -0600

    made adjustments to the apiroutes.js code to debug

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,10 @@
 const router = require('express').Router();
-const path = require('path');
 const fs = require('fs');
-let notesDB = require('../db/db.json');
+let notesdb = require('../db/db.json');
 
-const bodyParser = require('body-parser');
-router.use(bodyParser.json());
-
-const uuid = require('uuid');
-
-router.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
+router.get('/notes', function (_req, res) {
+    notesdb = JSON.parse(fs.readFileSync('../db/db.json'));
+    res.json(notesdb);
 });
 
 router.post('/notes', function (req, res) {
@@ -18,21 +13,23 @@ router.post('/notes', function (req, res) {
         text: req.body.text,
         id: Math.random(),
     };
-    newNotes.push (newNote);
+
+    notesdb.push (newNote);
     fs.writeFileSync('../db/db.json'), JSON.stringify(newNotes);
     res.json(newNotes);
 });
+
 router.delete('/notes/:id', function (req, res) {
     let newNotes = [];
-    for (let i = 0; i < notesDB.length; i++) {
-        if (notesDB[i].id !== req.params.id) {
-            newNotes.push(notesDB[i]);
+    for (var i = 0; i < notesdb.length; i++) {
+        if (notesdb[i].id !== req.params.id) {
+            newNotes.push(notesdb[i]);
         }
     }
 
-notesDB=newNotes;
-fs.writeFileSync('../db/db.json'), JSON.stringify(notesDB);
-res.json(notesDB);
+notesdb=newNotes;
+fs.writeFileSync('../db/db.json'), JSON.stringify(notesdb);
+res.json(notesdb);
 });
 
 module.exports = router;
