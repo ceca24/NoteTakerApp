d5486f01fff2dbf5501265fd30607f2bc7f17817
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-04 20:32:04 -0600

    needed to remove extra character from both routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let notesdb = require('../db/db.json');
+let notesdb = require('./db/db.json');
 
 var uuid = require('uuid');
 
@@ -8,7 +8,7 @@ module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
 
-        let data = fs.readFileSync('../db/db.json', 'utf8');
+        let data = fs.readFileSync('./db/db.json', 'utf8');
         
         notesdb = JSON.parse(data);
         
@@ -23,13 +23,13 @@ module.exports = (app) => {
             id: uuid.v4()
         };
 
-        let data = fs.readFileSync('../db/db.json', 'utf8');
+        let data = fs.readFileSync('./db/db.json', 'utf8');
 
         notesdb = JSON.parse(data);
 
         notesdb.push(newNote);
 
-        fs.writeFileSync('../db/db.json', JSON.stringify(notesdb));
+        fs.writeFileSync('./db/db.json', JSON.stringify(notesdb));
 
         res.json(notesdb);
 
@@ -37,13 +37,13 @@ module.exports = (app) => {
 
     app.delete('/api/notes/:id', (req, res) => {
 
-        let data = fs.readFileSync('../db/db.json', 'utf8');
+        let data = fs.readFileSync('./db/db.json', 'utf8');
 
         notesdb = JSON.parse(data);
 
         notesdb = notesdb.filter(note => note.id !== req.params.id);
 
-        fs.writeFileSync('../db/db.json', JSON.stringify(notesdb));
+        fs.writeFileSync('./db/db.json', JSON.stringify(notesdb));
 
         res.json(notesdb);
 
