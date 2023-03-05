2bd807581bd12b6d22cbd090edd68c9fa161424e
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-04 20:20:28 -0600

    made adjustments to htmlRoutes code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = function (app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/home.html)
-    // they are shown an HTML page of content
-    app.get("/", function (req, res) {
+    
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-    app.get('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
 
-    // If no matching route is found default to home
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 };
